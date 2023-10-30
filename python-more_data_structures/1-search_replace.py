e90d1a966d669c8aa72af7caac1a23bb5a29bde4
Manow <>
2023-10-30 21:42:48 +1100

    del char

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [replace if oldnumber == search else oldnumber for oldnumber in my_list]
+    new_list = [replace if oldnum == search else oldnum for oldnum in my_list]
     return new_list
