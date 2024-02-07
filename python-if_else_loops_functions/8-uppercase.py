9cf8f3d48cf89c36f31ceba59105fe368634de78
ManowItsaraphon <127257465+ManowItsaraphon@users.noreply.github.com>
2024-02-08 10:20:44 +1100

    Delete python-if_else_loops_functions directory

--- python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for ch in str:
-        c = ord(ch)
-        if ord('a') <= c <= ord('z'):
-            c = c - 32
-        print("{:c}".format(c), end="")
-    print()
--- python-if_else_loops_functions/9-print_last_digit.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def print_last_digit(number):
-    dig = abs(number) % 10
-    print("{:d}".format(dig), end="")
-    return (dig)
